82d012828e5b8b4f3ba5f7835a331182867ce6a8
wjr21 <wjr21@sussex.ac.uk>
2024-02-15 15:11:58 +0000

    Fully documented Tree

--- src/h5forest/tree.py
+++ src/h5forest/tree.py
@@ -1,10 +1,47 @@
+"""Tree class for the HDF5 file viewer.
+
+This module contains the Tree class which is used to represent the HDF5 file
+as a tree structure. The Tree contains Nodes which are used to represent the
+groups and datasets in the HDF5 file. Each Node is lazy loaded, meaning that
+its children are only loaded when it is opened.
+
+Example usage:
+    tree = Tree("example.h5")
+    print(tree.get_tree_text())
+"""
 import h5py
 
 from h5forest.node import Node
 
 
 class Tree:
+    """A class to represent the HDF5 file as a tree structure.
+
+    This class is used to represent the HDF5 file as a tree structure. The
+    tree is constructed from Node objects representing Groups and Datasets
+    which are lazy loaded, meaning that the children of a node are only
+    loaded when needed.
+
+    Attributes:
+        filepath (str):
+            The path to the HDF5 file.
+        roots (list):
+            A list of the root level nodes in the tree.
+        nodes_by_row (list):
+            A list of all nodes in the tree by row in the tree output.
+    """
+
     def __init__(self, filepath):
+        """
+        Initialise the tree.
+
+        This will set up the attributes we'll need and parses the root level of
+        the HDF5 file ready to be displayed.
+
+        Args:
+            filepath (str):
+                The path to the HDF5 file.
+        """
         # Store the file path we're working with
         self.filepath = filepath
 
@@ -41,6 +78,13 @@ class Tree:
         Parse the open nodes to produce the text tree representation.
 
         This will recurse through the open nodes constructing the output.
+
+        Returns:
+            str:
+                The text representation of the tree.
+            list:
+                A list containing the nodes where the index is the row
+                they are on in the text representation.
         """
         # Add this nodes representation
         text += f"{current_node.to_tree_string()}\n"
@@ -59,6 +103,18 @@ class Tree:
         return text, nodes_by_row
 
     def get_tree_text(self):
+        """
+        Return the text representation of the tree.
+
+        Note that this is only called at initialisation and thus only parses
+        the roots of the tree. Any future updates will be done via
+        update_tree_text. This is to avoid recalculating the full tree for
+        every change.
+
+        Returns:
+            str:
+                The text representation of the tree.
+        """
         text = ""
         nodes_by_row = []
         for root in self.roots:
@@ -95,7 +151,7 @@ class Tree:
 
     def close_node(self, node, current_row, tree_text_area):
         """Close the node."""
-        # Close the node itself and remove it from the tree
+        # Close the node itself
         node.close_node()
 
         # Get the tree text split into lines
@@ -121,5 +177,15 @@ class Tree:
         tree_text_area.text = "\n".join(tree_text)
 
     def get_current_node(self, row):
-        """Return the current node."""
+        """
+        Return the current node.
+
+        Args:
+            row (int):
+                The row in the tree.
+
+        Returns:
+            Node:
+                The node at row.
+        """
         return self.nodes_by_row[row]
