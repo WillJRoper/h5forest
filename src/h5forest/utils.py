6fe4e2c886a153d8e134a956cc63eb891ac15781
Will Roper <w.roper@sussex.ac.uk>
2025-11-09 21:30:41 +0000

    Revert "Fix width calculation by accounting for last label not needing padding"
    This reverts commit 40d52c0b9ff7d1921b206d8dba3928cce2e59cdf.

--- src/h5forest/utils.py
+++ src/h5forest/utils.py
@@ -138,10 +138,9 @@ class DynamicLabelLayout:
         max_label_width = max(label_widths) if label_widths else 20
 
         # Calculate how many labels can fit per row based on max width
-        # Each label takes max_label_width, but the last one doesn't need padding
-        # For N labels: N * text + (N-1) * padding = N * max_label_width - padding
-        # So: N <= (available_width + padding) / max_label_width
-        labels_per_row = max(1, (available_width + self.padding) // max_label_width)
+        # All labels will be padded to max_label_width, so we can fit:
+        # available_width // max_label_width labels per row
+        labels_per_row = max(1, available_width // max_label_width + 1)
 
         # Distribute labels evenly across rows in grid fashion
         rows = []
